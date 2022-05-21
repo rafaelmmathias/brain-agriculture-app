0abc37450d98e3a10c9f59160ecc5b3113014490
Rafael Mathias <mathias@inrecovery.org>
2022-05-21 14:27:24 -0300

    chore: updating readme.md

--- src/test/server/index.ts
+++ src/test/server/index.ts
@@ -1,9 +1,12 @@
 import codegen from 'codegen.macro'
 
-// using codegen here is useful because it means that when webpack or jest
-// handle this module, all they see is something like: "export * from './dev-server'"
-// so there's no need for dynamic require statements
-
+/**
+ * When running in dev, we use mock with dev-server
+ * When running tests, we use mock with test-server
+ * If is production mode, we do not mock anything.
+ * So, to run in production, make sure to have an API running
+ * on the .env file endpoint.
+ */
 
 codegen`
 if (process.env.NODE_ENV === 'development') {
@@ -11,13 +14,6 @@ if (process.env.NODE_ENV === 'development') {
 } else if (process.env.NODE_ENV === 'test') {
   module.exports = "export * from './test-server'"
 } else {
-  // in normal apps you'll not do anything in this case
-  // but for this workshop app, we're actually going to
-  // deploy our mock service worker to production
-  // so normally, this condition would just look like this:
-  // module.exports = ""
-  // but for us, since we're shipping the dev server to prod
-  // we'll do the same thing we did for development:
-  module.exports = "export * from './dev-server'"
+  module.exports = ""
 }
 `
